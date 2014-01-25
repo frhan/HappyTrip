dc8aff8ae53bbef9b0e9b376d337b0709ad6d4dd
farhan-faruque <russel.cse05@gmail.com>
2014-01-25 20:34:30 +0600

    db class added

---
+++ HappyTrip/src/com/farhan/happytrip/fragments/TopFragment.java
@@ -0,0 +1,19 @@
+package com.farhan.happytrip.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+public class TopFragment extends Fragment
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) 
+	{
+		super.onActivityCreated(savedInstanceState);
+	}
+
+}
