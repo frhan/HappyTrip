355f10726cf3872ffaa0131ac37e2c596db2f39d
farhan-faruque <russel.cse05@gmail.com>
2014-01-25 21:17:06 +0600

    list adapter added

---
+++ HappyTrip/src/com/farhan/happytrip/adapter/MemberListAdapter.java
@@ -0,0 +1,25 @@
+package com.farhan.happytrip.adapter;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import me.farhan.db.happytrip.Member;
+
+public class MemberListAdapter extends MainAdapter<Member>
+{
+
+	public MemberListAdapter(Context context, List<Member> items) {
+		super(context, items);
+		
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) 
+	{
+	
+		return null;
+	}
+
+}
